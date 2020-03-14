4c0bfce225b415b8d6faed371359a895c2f1dae5
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2020-03-14 17:15:57 -0600

    Same as random project but only random tempo

---
+++ AV5_NewProject_Random_Lite.lua
@@ -0,0 +1,23 @@
+--User Variables:
+--BASIC: Set to true or false to enable or disable feature
+RandomTempo = true
+--ADVANCED: Modify these values to limit the randomness
+tempo_range = {60,200}
+--[[
+Script: New Random Project Lite!
+
+Description: As u/Total-Jerk requested, just random tempo.
+
+Script by Alberto Valdez at av5sound.com and u/Sound4Sound
+--]]
+
+local function RandomTem()
+	local new_bpm = math.random(tempo_range[1],tempo_range[2])
+	reaper.SetCurrentBPM(0,new_bpm,false)
+	return new_bpm
+end
+
+--Process
+reaper.Main_OnCommand(40023, 0)
+bpm = reaper.Master_GetTempo()
+if RandomTempo == true then bpm = RandomTem() end
