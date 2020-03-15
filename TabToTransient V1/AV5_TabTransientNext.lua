2f0479507ed8baf352cc0e8cd5a5e9eb447071a2
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2020-03-15 17:15:45 -0600

    Tab to item if no item selected

--- TabToTransient V1/AV5_TabTransientNext.lua
+++ TabToTransient V1/AV5_TabTransientNext.lua
@@ -1,3 +1,3 @@
 tabToTransient = true
-if tabToTransient == true then reaper.Main_OnCommand(40375, 1)
+if tabToTransient == true and reaper.CountSelectedMediaItems(0) > 0 then reaper.Main_OnCommand(40375, 1)
 else reaper.Main_OnCommand(40319,1) end
--- TabToTransient V1/AV5_TabTransientPrev.lua
+++ TabToTransient V1/AV5_TabTransientPrev.lua
@@ -1,3 +1,3 @@
 tabToTransient = true
-if tabToTransient == true then reaper.Main_OnCommand(40376, 1)
+if tabToTransient == true and reaper.CountSelectedMediaItems(0) > 0 then reaper.Main_OnCommand(40376, 1)
 else reaper.Main_OnCommand(40318,1) end
