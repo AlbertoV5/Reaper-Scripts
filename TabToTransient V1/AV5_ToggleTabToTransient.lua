8599559d3735e90b4624db05fc6e503452c5eae2
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2020-03-17 00:01:21 -0600

    Toggle tab to transient
    Final version, fixed tab method and cleaned the script for efficiency.

--- TabToTransient V1/AV5_ToggleTabToTransient.lua
+++ TabToTransient V1/AV5_ToggleTabToTransient.lua
@@ -1,50 +1,46 @@
---User Variables
+--User Variables. 
+--Modify these if you change the name of the complementary files:
 fileNameTabPrev = "AV5_TabTransientPrev.lua"
 fileNameTabNext = "AV5_TabTransientNext.lua"
-enableDebug = false
+enableDebug = false --Enable to get info on your path, files and toggle
 --[[
 Script: Toggle Tab to transient
 
 Description: u/thatpaxguy requested a Toggle Tab to Transient. Fixed clunky method.
 
 V1.1: As of now, this works 100% on a Mac. Gotta test it on Win still.
+V1.2: Final version. Efficient and working as intended.
 
 Script by Alberto Valdez at av5sound.com and u/Sound4Sound
 --]]
 
 local function ReadFile(directory, fileName)
-	local allLines = {}
-	local file = io.open(directory..fileName, "r")
-	for line in file:lines() do 
-		table.insert(allLines,line) end
+	local file,allLines = io.open(directory..fileName, "r"),{}
 
+	for line in file:lines() do table.insert(allLines,line) end
 	if allLines[1] == "tabToTransient = false" then toggle = false
-	elseif allLines[1] == "tabToTransient = true" then toggle = true
-	end
+	elseif allLines[1] == "tabToTransient = true" then toggle = true end
+
 	if enableDebug == true then reaper.ShowConsoleMsg("\n\nTOGGLE: "..tostring(toggle)) end
 	file:close()
 end
 
 local function Toggle()
-	if toggle == true then
-		toggle = false
-		return "tabToTransient = false"
-	elseif toggle == false then
-		toggle = true
-		return "tabToTransient = true"
+	if toggle == true then toggle = false return "tabToTransient = false"
+	elseif toggle == false then toggle = true return "tabToTransient = true"
 	end
 end
 
 local function WriteFiles(directory)
 	local t = Toggle()
 	file = io.open(directory..fileNameTabPrev,"w")
-	line1 = t.."\nif tabToTransient == true and reaper.CountSelectedMediaItems(0) > 0 then reaper.Main_OnCommand(40376, 1)\nelse reaper.Main_OnCommand(40318,1) end"
+	line1 = t..textPrev
 	if enableDebug == true then reaper.ShowConsoleMsg("\n"..tostring(line1).."\n") end
 	file:write(line1)
 	file:close()
 
 	file2 = io.open(directory..fileNameTabNext,"w")
-	line2 = t.."\nif tabToTransient == true and reaper.CountSelectedMediaItems(0) > 0 then reaper.Main_OnCommand(40375, 1)\nelse reaper.Main_OnCommand(40319,1) end"
+	line2 = t..textNext
 	if enableDebug == true then reaper.ShowConsoleMsg("\n"..tostring(line2).."\n") end
 	file2:write(line2)
 	file2:close()
@@ -56,31 +52,16 @@ local function GetDirectory()
 	return info.source:match[[^@?(.*[\\/])[^\\/]-$]]
 end
 
-local function GetOS()
-	local userOS = reaper.GetOS()
-	if userOS == "OSX64" or userOS == "OSX32" then
-		return "/"
-	elseif userOS == "Win32" or userOS == "Win64" then
-		return "\\"
-	else
-		return "/"
-	end
-end
-
-function mysplit (inputstr, sep) --from strackoverflow user973713
-        local t={}
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-                table.insert(t, str)
-        end
-        return t
-end
-
---Process
+--Process. Saving the text on the complementary files here for safety.
 
 newDir = GetDirectory()
 
-s = tostring(GetOS())
 if enableDebug == true then reaper.ShowConsoleMsg("\nScript Directory: "..newDir) end
-
 ReadFile(newDir, fileNameTabNext)
+
+textNext = "\nif tabToTransient == true and reaper.CountSelectedMediaItems(0) > 0 then reaper.Main_OnCommand(40375, 1)"..
+"\nelse reaper.Main_OnCommand(40421, 1) reaper.Main_OnCommand(40319,1) reaper.SelectAllMediaItems(0, false) end"
+textPrev = "\nif tabToTransient == true and reaper.CountSelectedMediaItems(0) > 0 then reaper.Main_OnCommand(40376, 1)"..
+"\nelse reaper.Main_OnCommand(40421, 1) reaper.Main_OnCommand(40318,1) reaper.SelectAllMediaItems(0, false) end"
+
 WriteFiles(newDir)
