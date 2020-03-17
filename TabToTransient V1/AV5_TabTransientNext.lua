2cb24c274deb56f1eba2016679c2cee3f514c6a2
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2020-03-17 01:02:55 -0500

    final version, new tab method

--- TabToTransient V1/AV5_TabTransientNext.lua
+++ TabToTransient V1/AV5_TabTransientNext.lua
@@ -1,3 +1,3 @@
 tabToTransient = true
 if tabToTransient == true and reaper.CountSelectedMediaItems(0) > 0 then reaper.Main_OnCommand(40375, 1)
-else reaper.Main_OnCommand(40319,1) end
+else reaper.Main_OnCommand(40421, 1) reaper.Main_OnCommand(40319,1) reaper.SelectAllMediaItems(0, false) end
